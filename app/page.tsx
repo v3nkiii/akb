ebcf6fab8b63e7469080f6dff490ee21b223b30d
v3nkiii <v3nkiii@users.noreply.github.com>
2026-07-03 04:10:24 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -13,9 +13,16 @@ export default function Home() {
         backgroundRepeat: "no-repeat",
       }}
     >
-      <div className="absolute inset-0 bg-black/10" />
+      {/* DARK OVERLAY */}
+      <div className="absolute inset-0 bg-black/20" />
 
-      <div className="absolute left-1/2 top-[76%] -translate-x-1/2">
+      {/* BUTTON WRAPPER */}
+      <div
+        className="absolute left-1/2 -translate-x-1/2"
+        style={{
+          top: "clamp(65%, 72%, 78%)",
+        }}
+      >
         <Link
           href="/about"
           className="
@@ -26,9 +33,9 @@ export default function Home() {
             border-[3px]
             border-yellow-400
             bg-[#07183D]/90
-            px-14
-            py-5
-            text-3xl
+            px-8 sm:px-14
+            py-3 sm:py-5
+            text-xl sm:text-3xl
             font-bold
             text-yellow-300
             shadow-[0_0_35px_rgba(212,175,55,.45)]
@@ -36,6 +43,7 @@ export default function Home() {
             duration-300
             hover:scale-105
             hover:bg-[#0A255A]
+            text-center
           "
         >
           ▶ START SHOW
